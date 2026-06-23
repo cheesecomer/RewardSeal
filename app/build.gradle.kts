14338323706478f320455175f3794be9108e43a6
cheesecomer <orz.pgr.m9@gmail.com>
2026-06-23 20:53:19 +0900

    chore: bump version to 0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.cheesecomer.rewardstamp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.9.0"
+        versionCode = 2
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
