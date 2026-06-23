4dcae0f884acaffedf46e04ddcb6b8ccd798cac1
cheesecomer <orz.pgr.m9@gmail.com>
2026-06-23 20:44:33 +0900

    fix: disable dark mode and edge-to-edge layout

--- app/src/main/java/com/cheesecomer/rewardstamp/ui/theme/Theme.kt
+++ app/src/main/java/com/cheesecomer/rewardstamp/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.cheesecomer.rewardstamp.ui.theme
 
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -62,7 +61,7 @@ private val LightColorScheme =
 
 @Composable
 fun RewardStampTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
